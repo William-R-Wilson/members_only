fc81600c2f78bc1b18de4cfa150199a1991c1f1d
Will <williamrwilson@gmail.com>
2015-03-06 23:54:53 +0000

    add login and show views and login method, add remember_token to Users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,14 @@ class SessionsController < ApplicationController
   def new
     
   end
+  
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user.authenticate(params[:session][:password_digest])
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+    
 end
